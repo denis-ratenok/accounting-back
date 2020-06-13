984acb2aa707cdf5dd82873a7732ac4500e4c3f9
DenisR <denis.ratenok@gmail.com>
2020-06-13 15:59:31 +0300

    Add ui

---
+++ src/main/resources/static/precache-manifest.506edd9cd5a60cf292e568058d3ab0a4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "477230c5c0874c7f101305a8bfd828eb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "85445477dabe6ffaa8db",
+    "url": "/static/css/main.47958357.chunk.css"
+  },
+  {
+    "revision": "bb5683776ffaef3e77e5",
+    "url": "/static/js/2.1052dfde.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.1052dfde.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "85445477dabe6ffaa8db",
+    "url": "/static/js/main.8915cd58.chunk.js"
+  },
+  {
+    "revision": "54f397d624cf3cd27d61",
+    "url": "/static/js/runtime-main.3902d768.js"
+  }
+]);
